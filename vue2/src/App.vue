02eba0eca5e35f499b029f65f8c2bb729f78e3a1
komorebi920 <yxc900920@163.com>
2024-04-26 13:50:28 +0800

    feat: vue2 应用功能改造

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{ num }}</div>
+    <Button @change="onChange" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from "./components/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Button
+  },
+  data() {
+    return {
+      num: 0
+    };
+  },
+  methods: {
+    onChange() {
+      this.num++;
+    }
   }
-}
+};
 </script>
 
 <style>
---
+++ vue2/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button type="button" @click="onClick">+1</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  methods: {
+    onClick() {
+      this.$emit("change");
+    }
+  }
+};
+</script>
